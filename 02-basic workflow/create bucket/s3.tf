e26bee6d54b54803a3731543a2efc2098457ac17
Alves0611 <g.alves0611@gmail.com>
2024-08-03 20:35:37 -0300

    feat: add versioning configuration to bucket resource

--- 02-basic workflow/create bucket/s3.tf
+++ 02-basic workflow/create bucket/s3.tf
@@ -6,3 +6,10 @@ resource "aws_s3_bucket" "this" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
