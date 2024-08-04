8d3f5ee77b08047d582b0faa32829f7f1955280a
Alves0611 <g.alves0611@gmail.com>
2024-08-03 23:32:35 -0300

    feat: add variables for read capacity, write capacity, and deletion protection

--- 03-configuration language/02-variables/variables.tf
+++ 03-configuration language/02-variables/variables.tf
@@ -3,3 +3,23 @@ variable "aws_region" {
   description = "The region to deploy the infra to"
   default     = "us-east-1"
 }
+
+variable "read_capacity" {
+  type        = number
+  description = ""
+  default     = 5
+}
+
+variable "write_capacity" {
+  type        = number
+  description = ""
+  default     = 5
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = ""
+  default     = false
+}
+
+
