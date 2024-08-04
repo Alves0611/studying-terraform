e3cec4921c5cc9b5380f565235bcfbfbde98402d
Alves0611 <g.alves0611@gmail.com>
2024-08-04 19:04:07 -0300

    feat: update output configuration to include additional local variables

--- 03-configuration language/05-interpolation/outputs.tf
+++ 03-configuration language/05-interpolation/outputs.tf
@@ -3,5 +3,11 @@ output "locals" {
     literal_string    = local.literal_string
     concatened_string = local.concatened_string
     function_string   = local.function_string
+
+    heredoc_string          = local.heredoc_string
+    indented_heredoc_string = local.indented_heredoc_string
+
+    json         = local.json
+    decoded_json = local.decoded_json["foo"]
   }
 }
