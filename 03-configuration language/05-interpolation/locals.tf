387988b488d9a219718b6912b7f011f30d6fa2a6
Alves0611 <g.alves0611@gmail.com>
2024-08-04 19:02:46 -0300

    feat: add Heredoc strings and JSON/YAML encoding/decoding to local variables configuration

--- 03-configuration language/05-interpolation/locals.tf
+++ 03-configuration language/05-interpolation/locals.tf
@@ -2,4 +2,26 @@ locals {
   literal_string    = "Isto é uma string literal, sendo uma sequência de caracteres dentro de aspas duplas"
   concatened_string = "${random_pet.string.id}-texto-aleatório-${var.environment}"
   function_string   = replace(local.literal_string, "uma", "UMA")
+
+  # Heredoc
+  heredoc_string = <<EOT
+  hello
+  world
+  EOT
+
+  indented_heredoc_string = <<-EOT
+  hello
+    world
+      Terraform
+  EOT
+
+  # JSON or YML
+  # https://developer.hashicorp.com/terraform/language/expressions/strings#generating-json-or-yaml
+  json = jsonencode({
+    hello = "world"
+    foo   = "bar"
+    a     = "b"
+  })
+  decoded_json = jsondecode(local.json)
 }
+
