7a438044b7eba4aa2e07934f3cec34e89ef46bc7
Alves0611 <g.alves0611@gmail.com>
2024-08-05 19:54:06 -0300

    feat: update output values to include collection, encoding, and type conversion manipulations

--- 03-configuration language/13-built-in-functions/outputs.tf
+++ 03-configuration language/13-built-in-functions/outputs.tf
@@ -6,5 +6,21 @@ output "locals" {
       length  = local.length
       regex   = local.regex
     }
+    collection = {
+      contains = local.contains
+      element  = local.element
+      index    = local.index
+      keys     = local.keys
+      lookup   = local.lookup
+    }
+    encoding = {
+      jsonencode   = local.jsonencode
+      base64encode = local.base64encode
+      template     = local.template
+    }
+    type = {
+      can = local.can
+      try = local.try
+    }
   }
 }
