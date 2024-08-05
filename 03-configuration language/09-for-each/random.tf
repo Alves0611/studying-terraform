0a366b4c8be256b7633cefd58356ff41f0a3d27b
Alves0611 <g.alves0611@gmail.com>
2024-08-05 08:38:48 -0300

    feat: add random_pet resource with for_each configuration

---
+++ 03-configuration language/09-for-each/random.tf
@@ -0,0 +1,10 @@
+resource "random_pet" "for_each" {
+  for_each = var.create_random_pets ? {
+    dog  = 4
+    cat  = 2
+    bird = 3
+    pig  = 5
+  } : {}
+  length = each.value
+  prefix = each.key
+}
