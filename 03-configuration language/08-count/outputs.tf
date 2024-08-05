ea1b8d50a52ce9c1f16c78e68f866786183e5e18
Alves0611 <g.alves0611@gmail.com>
2024-08-05 08:33:15 -0300

    feat: update pets output to conditionally include pet IDs based on create_random_pets variable

--- 03-configuration language/08-count/outputs.tf
+++ 03-configuration language/08-count/outputs.tf
@@ -1,8 +1,8 @@
 output "pets" {
-  value = [
+  value = var.create_random_pets ? [
     random_pet.count[0].id,
     random_pet.count[1].id,
     random_pet.count[2].id,
     random_pet.count[3].id
-  ]
+  ] : null
 }
