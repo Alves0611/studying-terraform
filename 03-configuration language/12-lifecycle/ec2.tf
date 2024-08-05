6b58c587d6d413b60a6c9fb71c80785ebaf01b8d
Alves0611 <g.alves0611@gmail.com>
2024-08-05 09:26:48 -0300

    feat: add preconditions and postconditions to aws_instance resource

--- 03-configuration language/12-lifecycle/ec2.tf
+++ 03-configuration language/12-lifecycle/ec2.tf
@@ -21,6 +21,18 @@ resource "aws_instance" "this" {
       null_resource.replacement_trigger
     ]
     ignore_changes = [tags]
-    
+
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.ubuntu.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+
+    # The EC2 instance must be allocated a public DNS hostname.
+    postcondition {
+      condition     = self.public_dns != ""
+      error_message = "EC2 instance must be in a VPC that has public DNS hostnames enabled."
+    }
   }
 }
