a021f7db62e5debd297fb3deac9b9f3fe7ff9adc
Alves0611 <g.alves0611@gmail.com>
2024-08-06 00:46:18 -0300

    feat: add Terraform configuration for required AWS provider and detailed S3 backend

---
+++ 04-remote-state/05-outputs/02-ec2/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.22.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "tfstate-891377404175"
+    key            = "dev/05-outputs/instances/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tflock-tfstate-891377404175"
+  }
+}
